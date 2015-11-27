4519180e600757c804e3db2669e648aa99f4cbca
Morgan Owen <mog13@live.com>
2015-11-27 20:46:42 +0000

    initial framework scaffolding

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name timelyApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the timelyApp
+ */
+angular.module('timelyApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
